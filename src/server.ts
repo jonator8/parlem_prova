462603fea9c57697abdd739ce634a1b682dd909c
jonator <jonathan.nebot@lvp.global>
2021-02-05 13:29:35 +0100

    change server to get controllers

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,25 @@
 import express from "express"
 import {createConnection} from "typeorm";
+import {getAppControllers} from "./controllers";
 
 createConnection().then(connection => {
     // create and setup express app
     const app = express();
 
-    const port = 3000;
+    const port = process.env.PORT || 3000;
+
     app.get('/', (req, res) => {
         res.send('Hello World!')
     });
 
-// start express server
-    app.listen(port, () => {
+    getAppControllers().then((controllers) => {
+        controllers.map((controller) => {
+            app.use('/api/v1/', controller.router);
+        });
+    });
+
+        // start express server
+    app.listen(Number(port), () => {
         console.log(`Example app listening at http://localhost:${port}`)
     });
 });
