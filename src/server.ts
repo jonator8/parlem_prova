4cf6a71d0c70722a758794a03ca67dd8ba0a7020
jonator <jonathan.nebot@lvp.global>
2021-02-05 12:28:27 +0100

    adding typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from "express"
+import {createConnection} from "typeorm";
 
-// create and setup express app
-const app = express();
+createConnection().then(connection => {
+    // create and setup express app
+    const app = express();
 
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+    const port = 3000;
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    });
 
 // start express server
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    });
 });
