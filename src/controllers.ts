37354b03cf11bd904555190fd010b914380deb85
jonator <jonathan.nebot@lvp.global>
2021-02-05 15:15:23 +0100

    add ProductController to controllers

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,7 +1,9 @@
 import CustomerController from "./controllers/CustomerController";
+import ProductController from "./controllers/ProductController";
 
 export async function getAppControllers() {
     return [
-        new CustomerController()
+        new CustomerController(),
+        new ProductController()
     ];
 }
